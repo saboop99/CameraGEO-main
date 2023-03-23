dc6482cc9281c37101e96003ba19ac3886bb3955
saboop99 <rafa4021@hotmail.com>
2023-03-23 17:09:08 -0300

    Start writing the MainActivity

--- app/src/main/java/com/facens/myapplication/MainActivity.java
+++ app/src/main/java/com/facens/myapplication/MainActivity.java
@@ -1,14 +1,26 @@
 package com.facens.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView ImageViewFoto;
+    private Button btnGeo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnGeo = (Button) findViewById(R.id.btn_gps);
+        ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION}, 123);
+
+
     }
-}//test
+}
